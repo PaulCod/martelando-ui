b921af96b6327342bdd63cd8cb1d99b72984e102
Paulo <paulolds086543@gmail.com>
2025-03-23 22:00:50 -0300

    feat: Project struct

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { MakeOfferComponent } from './pages/make-offer/make-offer.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "product/{id}",
+    component: ProductComponent
+  },
+  {
+    path: "profile",
+    component: ProfileComponent
+  },
+  {
+    path: "makeoffer",
+    component: MakeOfferComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: "**",
+    component: NotFoundComponent
+  }
+];
---
+++ src/app/pages/make-offer/make-offer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-make-offer',
+  imports: [],
+  templateUrl: './make-offer.component.html',
+  styleUrl: './make-offer.component.css'
+})
+export class MakeOfferComponent {
+
+}
